964b2a61ab90b6168c772d487833bd698df07794
Suroj <surojmaharjan0@gmail.com>
2020-06-03 12:17:32 +0545

    HTTPS

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -10,7 +10,7 @@ const getCity = async (city) => {
 }
 
 const getWeather = async (city_key) => {
-    const base_url = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const base_url = 'https://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${city_key}?apikey=${API_KEY}`;
 
     const response = await fetch(base_url + query);
